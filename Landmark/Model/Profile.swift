6266d1cb644bc7fed9e31cd9737770ef236b1959
infinity <egold92@yandex.ru>
2021-01-29 10:43:07 +0700

    init

---
+++ Landmark/Model/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  Landmark
+//
+//  Created by Александр Котляров on 10.01.2021.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "g_kumar")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+        var id: String { self.rawValue }
+    }
+}
